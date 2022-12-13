ed1977178962db3eca17486ba36dbeefa9b401c0
purwar1997 <shubhampurwar511@gmail.com>
2022-12-14 01:56:33 +0530

    project updated

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ const router = require("./routes/router");
 const express = require("express");
 const app = express();
 
+// used to parse JSON data that comes via POST and PUT request
+app.use(express.json());
+
+// used to parse urlencoded data that comes via GET request
+app.use(express.urlencoded({ extended: true }));
+
 // router object acts like a middleware
 app.get("/", router);
 app.get("/getUsers", router);
+app.post("/createUser", router);
+app.get("/getUser/:userId", router);
+app.put("/updateUser/:userId", router);
+app.delete("/deleteUser/:userId", router);
 
 module.exports = app;
--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const { home, getUsers } = require("../controllers/routehandlers");
+
+const {
+  home,
+  getUsers,
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/routehandlers");
 
 // router object is used to handle incoming requests
 router.get("/", home);
 router.get("/getUsers", getUsers);
+router.post("/createUser", createUser);
+router.get("/getUser/:userId", getUser);
+router.put("/updateUser/:userId", updateUser);
+router.delete("/deleteUser/:userId", deleteUser);
 
 module.exports = router;
